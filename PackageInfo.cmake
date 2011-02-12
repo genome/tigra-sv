029ba0070ab39063b31633df87fea8a7e1b2b861
Travis Abbott <tabbott@genome.wustl.edu>
2011-02-11 18:33:23 -0600

    add real tigra-sv source code

--- PackageInfo.cmake
+++ PackageInfo.cmake
@@ -8,13 +8,26 @@ endif ()
 
 # The format of the description field is a short summary line followed by a
 # longer paragraph indented by a single space on each line
-set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "An example tool
- This example tool serves as a template for your c or
- c++ project")
-set(CPACK_PACKAGE_NAME "example${EXE_VERSION_SUFFIX}")
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY
+"Tigra_sv is a program that conducts targeted local assembly of structural variants.
+ Tigra_sv is a program that conducts targeted local assembly of structural
+ variants (SV) using the iterative graph routing assembly (TIGRA) algorithm (L.
+ Chen et al, unpublished). It takes as input a list of putative SV calls and a
+ set of bam files that contain reads mapped to a reference genome such as NCBI
+ build36.  For each SV call, it assembles the set of reads that were mapped or
+ partially mapped to the region of interest (ROI) in the corresponding bam
+ files. Instead of outputing a single consensus sequence, tigra_sv attempts to
+ construct all the alternative alleles in the ROI as long as they received
+ sufficient sequence coverage (usually >= 2x).  It also utilizes the variant
+ type information in the input files to select reads for assembly.  Tigra_sv is
+ effective at improving the SV prediction accuracy and resolution in short reads
+ analysis and can produce accurate breakpoint sequences that are useful to
+ understand the origin, mechanism and pathology underlying the SVs.")
+
+set(CPACK_PACKAGE_NAME "tigra-sv${EXE_VERSION_SUFFIX}")
 set(CPACK_PACKAGE_VENDOR "wugc")
 set(CPACK_PACKAGE_VERSION ${FULL_VERSION})
-set(CPACK_DEBIAN_PACKAGE_MAINTAINER "Some Body <somebody@genome.wustl.edu>")
+set(CPACK_DEBIAN_PACKAGE_MAINTAINER "Ken Chen <kchenken@gmail.com>")
 set(CPACK_SYSTEM_NAME "Linux-${ARCH}")
 set(CPACK_TOPLEVEL_TAG "Linux-${ARCH}")
 set(CPACK_DEBIAN_PACKAGE_SECTION science)
